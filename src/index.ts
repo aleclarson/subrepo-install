e49175c163bc0329ea602468c11da018105e9819
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-04-14 17:21:53 -0400

    feat: variety pack

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import createDebug from 'debug'
-import { existsSync, readFileSync, rmSync, symlinkSync } from 'node:fs'
+import crypto from 'node:crypto'
+import {
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  rmSync,
+  symlinkSync,
+  writeFileSync,
+} from 'node:fs'
 import path from 'node:path'
 import c from 'picocolors'
 import { spawnSync as $ } from 'picospawn'
-import { isObject, isString } from 'radashi'
+import { concat, isObject, isString } from 'radashi'
 
 const debug = createDebug('subrepo-install')
 const log = (msg: string) => console.log(c.magenta(c.italic(msg)))
@@ -35,10 +43,18 @@ export interface Subrepo {
    */
   packages?: string[]
   /**
+   * What to do with the root package.
+   *
+   * - `ignore`: Do nothing with the root package.
+   * - `install-only`: Install dependencies but don't build or link the root package.
+   * - `default`: Treat the root package like any other package.
+   */
+  rootPackage?: 'ignore' | 'install-only' | 'default'
+  /**
    * If a sub-repo exists elsewhere when in the context of a
    * workspace, you can link to this path instead of cloning.
    */
-  workspaceDir?: string
+  workspaceOverride?: string
   /**
    * If a sub-repo has `node_modules` dependencies that you also want
    * in the `node_modules` of this repo, list them here.
@@ -48,14 +64,14 @@ export interface Subrepo {
    * Create symlinks for files in the sub-repo to the root of this repo.
    */
   linkFiles?: Record<string, string>
-  /**
-   * Skip the `build` script in the sub-repo's root `package.json`.
-   */
-  skipRootBuild?: boolean
 }
 
 export default function subrepoInstall(repos: Subrepo[]) {
-  const isInWorkspace = $('pnpm root -w', { exit: false }).status === 0
+  const isInWorkspace =
+    $('pnpm root -w', {
+      stdio: 'ignore',
+      exit: false,
+    }).status === 0
 
   const hasLockfile = (dir: string) =>
     existsSync(path.join(dir, 'pnpm-lock.yaml'))
@@ -63,13 +79,20 @@ export default function subrepoInstall(repos: Subrepo[]) {
   const isWorkspace = (dir: string) =>
     existsSync(path.join(dir, 'pnpm-workspace.yaml'))
 
+  const metadataPath = '.cache/subrepo-install.json'
+  const metadata = readJsonFile<Metadata>(metadataPath) ?? {}
+
+  const oldHeads = metadata.heads
+  const newHeads: typeof metadata.heads = {}
+
   for (const repo of repos) {
-    if (isInWorkspace && repo.workspaceDir) {
-      ensureSymlink(repo.dir, repo.workspaceDir)
+    if (isInWorkspace && repo.workspaceOverride) {
+      ensureSymlink(repo.dir, repo.workspaceOverride)
       continue
     }
 
     let ref = repo.ref
+    let head: string
     let shouldUpdate = true
 
     if (existsSync(repo.dir)) {
@@ -77,17 +100,17 @@ export default function subrepoInstall(repos: Subrepo[]) {
         stdio: 'pipe',
       })
 
-      const currentHead = $('git -C %s rev-parse HEAD', [repo.dir], {
+      head = $('git -C %s rev-parse HEAD', [repo.dir], {
         stdio: 'pipe',
       })
 
-      const desiredHead = isCommitHash(ref)
+      const expectedHead = isCommitHash(ref)
         ? ref
         : $('git -C %s ls-remote origin %s', [repo.dir, ref], {
             stdio: 'pipe',
           }).slice(0, 40)
 
-      if (currentHead === desiredHead) {
+      if (head === expectedHead) {
         shouldUpdate = false
       }
 
@@ -107,32 +130,86 @@ export default function subrepoInstall(repos: Subrepo[]) {
       $('git -C %s reset --hard FETCH_HEAD', [repo.dir])
     }
 
-    const repoIsWorkspace = isWorkspace(repo.dir)
+    head = $('git -C %s rev-parse HEAD', [repo.dir], {
+      stdio: 'pipe',
+    })
 
-    for (const name of ['', repo.packages].flat().filter(x => x != null)) {
+    const rootIsWorkspace = isWorkspace(repo.dir)
+    const rootPackageStrategy = repo.rootPackage ?? 'default'
+    const rootPackageId =
+      rootPackageStrategy !== 'ignore' &&
+      existsSync(path.join(repo.dir, 'package.json'))
+        ? '.'
+        : null
+
+    for (const name of concat(rootPackageId, repo.packages)) {
       const packageDir = path.join(repo.dir, name)
-      const isRootPackage = name === ''
+      const isRootPackage = name === '.'
+      const headChanged = head !== metadata.heads?.[packageDir]
+
+      let shouldTrackHead = false
 
       if (isRootPackage || !isWorkspace(repo.dir)) {
-        log(`Installing dependencies for ${packageDir}...`)
-        $('pnpm -C %s install', [
-          packageDir,
-          // Avoid generating a lockfile if none exists yet.
-          !hasLockfile(packageDir) && '--no-lockfile',
-          // Avoid using a workspace unrelated to this clone.
-          !repoIsWorkspace && !isWorkspace(packageDir) && '--ignore-workspace',
-        ])
+        shouldTrackHead = true
+        if (headChanged) {
+          log(`Installing dependencies for ${packageDir}...`)
+          $('pnpm -C %s install', [
+            packageDir,
+            // Avoid generating a lockfile if none exists yet.
+            !hasLockfile(packageDir) && '--no-lockfile',
+            // Avoid using a workspace unrelated to this clone.
+            !rootIsWorkspace &&
+              !isWorkspace(packageDir) &&
+              '--ignore-workspace',
+          ])
+        }
       }
 
-      if (!isRootPackage || !repo.skipRootBuild) {
-        log(`Building ${packageDir} if needed...`)
-        $('pnpm -C %s run --if-present build', [packageDir])
+      if (!isRootPackage || rootPackageStrategy !== 'install-only') {
+        const pkg = readPackageJson(packageDir)
+
+        if (pkg?.scripts?.build) {
+          shouldTrackHead = true
+          if (headChanged) {
+            log(`Building ${formatRelative(packageDir)}...`)
+            $('pnpm -C %s run build', [packageDir])
+          }
+        }
+
+        if (pkg?.name) {
+          ensureSymlink(
+            path.join(repo.dir, 'node_modules', pkg.name),
+            packageDir
+          )
+        }
+      }
+
+      if (shouldTrackHead) {
+        newHeads[packageDir] = head
+
+        if (headChanged) {
+          saveJsonFile(metadataPath, {
+            ...oldHeads,
+            ...newHeads,
+          })
+        } else {
+          debug(`Nothing changed with ${formatRelative(packageDir)}`)
+        }
       }
     }
+
+    // Drop metadata for packages no longer in the repo.
+    saveJsonFile(metadataPath, {
+      ...metadata,
+      heads: newHeads,
+    })
 
     for (const name of repo.inheritDependencies ?? []) {
       const targetDir = path.join(repo.dir, 'node_modules', name)
       const targetPkg = readPackageJson(targetDir)
+      if (!targetPkg) {
+        continue
+      }
 
       ensureSymlink(path.join('node_modules', name), targetDir)
 
@@ -159,20 +236,41 @@ export default function subrepoInstall(repos: Subrepo[]) {
   }
 }
 
+interface Metadata {
+  /** The last seen commit hash for each package. */
+  heads?: Record<string, string>
+}
+
 interface PackageJson extends Record<string, unknown> {
   name: string
   bin?: string | Record<string, string>
+  scripts?: Record<string, string>
 }
 
-function readPackageJson(dir: string): PackageJson {
-  return JSON.parse(readFileSync(path.join(dir, 'package.json'), 'utf-8'))
+function readPackageJson(dir: string): PackageJson | null {
+  return readJsonFile(path.join(dir, 'package.json'))
+}
+
+function readJsonFile<T>(file: string): T | null {
+  try {
+    return JSON.parse(readFileSync(file, 'utf-8'))
+  } catch (error) {
+    debug(`Error reading ${formatRelative(file)}: %s`, error)
+    return null
+  }
+}
+
+function saveJsonFile(file: string, data: unknown) {
+  mkdirSync(path.dirname(file), { recursive: true })
+  writeFileSync(file, JSON.stringify(data, null, 2))
 }
 
 function ensureSymlink(from: string, to: string) {
   if (debug.enabled) {
     debug(`Linking ${formatRelative(from)} to ${formatRelative(to)}`)
   }
-  rmSync(from, { force: true })
+  rmSync(from, { recursive: true, force: true })
+  mkdirSync(path.dirname(from), { recursive: true })
   symlinkSync(path.relative(path.dirname(from), to), from)
 }
 
@@ -183,4 +281,8 @@ function formatRelative(file: string) {
 
 function isCommitHash(ref: string) {
   return /^[0-9a-f]{40}$/.test(ref)
+}
+
+function md5Hash(str: string) {
+  return crypto.createHash('md5').update(str).digest('hex')
 }
